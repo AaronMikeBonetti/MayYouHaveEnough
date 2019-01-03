9c768d1e752a0142ecdc0a56baf2b17320931166
Aaron Bonetti <aaronmikebonetti@gmail.com>
2019-01-03 08:39:45 -0500

    #7 Commit: Auto Slideshow, started Media Arrow Functions

--- MayYouHaveEnough/MayYouHaveEnough.js
+++ MayYouHaveEnough/MayYouHaveEnough.js
@@ -2,105 +2,128 @@
 //:::::::::::::::::::Nav Toggler Animation::::::::::::
 
 function navToggle(){
-    
     let mainNav = document.getElementById("main-nav");
     mainNav.classList.toggle("nav-open")
 } 
 
 //:::::::::::::::::::Media Animations::::::::::::
+var imgsArray = [
+    {url: "url('Pics/colorado1.jpg')", id: 0},
+    {url: "url('Pics/colorado2.jpg')", id: 1},
+    {url: "url('Pics/colorado6.jpg')", id: 2},
+    {url: "url('Pics/colorado4.jpg')", id: 3},
+    {url: "url('Pics/colorado5.jpg')", id: 4},
+]
+var mediaMain = document.getElementsByClassName("media-main")[0];  
 
-function displayToMain(num) {
-    
-    let mediaMain = document.getElementsByClassName("media-main")[0];
-   
-    let img = url => {
-        mediaMain.style.backgroundImage = `${url}` ;   
-        }
-    
-    let transition = () =>{
-    mediaMain.classList.add("media-transition") 
-    
-        setTimeout(function(){
-            mediaMain.classList.remove("media-transition")
-        }, 500)
+var pushUrlToMain = url => {
+    mediaMain.style.backgroundImage = `${url}`; 
+    }
+var count = 0;
 
-   }
-   transition()
-  
-    imgsArray = [
-        {img: "url('Pics/colorado1.jpg')", id: 0},
-        {img: "url('Pics/colorado2.jpg')", id: 1},
-        {img: "url('Pics/colorado6.jpg')", id: 2},
-        {img: "url('Pics/colorado4.jpg')", id: 3},
-        {img: "url('Pics/colorado5.jpg')", id: 4},
-    ]
+function checkCount(){
+if(count >= 5){
+     return count = 0
+}
+if(count <= 0){
+     return count = 5
+}
+}
+checkCount()
 
+function transition(element){
+    element.classList.add("media-transition") 
+    setTimeout(function(){
+    element.classList.remove("media-transition")
+    }, 500)
+}
+
+function displayToMain(num) { 
+    checkCount()
+    transition(mediaMain)
+    
      for(imgs in imgsArray){
         if(imgsArray.id = num + 1){
-        return img(imgsArray[num].img)
+         pushUrlToMain(imgsArray[num].url)
+         count = num
             }
+
+     }
+     
+}
+function ArrowImgChange(direction) { 
+    checkCount()
+     
+    transition(mediaMain)
+   
+    if(direction= "right"){
+     
+        for(imgs in imgsArray){
+        
+            if(mediaMain.style.backgroundImage = `url('Pics/colorado${count}.jpg')`){
+               
+                 pushUrlToMain(imgsArray[count++].url)
+                 count++
+                
+            }
+           
+        }
+     }
+    if(direction= "left"){
+     
+        for(imgs in imgsArray){
+            if(mediaMain.style.backgroundImage = `url('Pics/colorado${count}.jpg')`){
+                count--
+                 pushUrlToMain(imgsArray[count].url)
+                
+            }
+            
+        }
      }
 }
 
 
-//:::::::::::::Change Img based on current img and on a timer
+//:::::::::::::SlideShow::::::::::::::::://
 
-//function for finding what picture is currently in media main;
-// if(indexof = id) 
 
-// function ArrowImgChange(direction){
-//     let mediaMain = document.getElementsByClassName("media-main")[0];
-//     let index = 0;
-//     let iterations = 0;
 
-//     imgsArray = [
-//         {img: "url('Pics/colorado1.jpg')", id: 0},
-//         {img: "url('Pics/colorado2.jpg')", id: 1},
-//         {img: "url('Pics/colorado6.jpg')", id: 2},
-//         {img: "url('Pics/colorado4.jpg')", id: 3},
-//         {img: "url('Pics/colorado5.jpg')", id: 4},
-//     ]
+function SlideShow(){
+    checkCount()
     
-//     if(direction === "right"){
-//         console.log("right!")
-//     }
-//     if(direction === "left"){
-//         console.log("left!")
-//     }
+    function runCarousel(){ 
 
-   
-//     let img = url => {
-//         mediaMain.style.backgroundImage = `${url}` ;   
-//         }
-     
-     
-//      var changeColor = function () {
-//          if (index >= imgsArray.length) {
-//              index = 0;
-//              iterations++;
-//          }
-//           = imgsArray.id[index];
-    //      //	stem.style.backgroundColor = colors[index];
-    //      if (iterations >= colors.length) {
-    //          iterations = 0;
-    //          clearInterval(setInterval);
-    //      } else {
-    //          index++;
-    //      }
-    //  }
-    //  setInterval(changeColor, 1000);
-     
     
-        // let mediaMain = document.getElementsByClassName("media-main")[0];
 
-        // for(imgs in imgsArray){
-        //     if(imgsArray.id = num + 1){
-        //         return img(imgsArray[num].img)
-//         //     }
-// }
+    if(count >= imgsArray.length){
+            count = 0
+            iteration = 0
+    }
+     if(count <  imgsArray.length){
+    pushUrlToMain(imgsArray[count].url)
+    transition(mediaMain)
+        count++
+        iteration++
+      }  
+      
+    }
+
+if(displayedToMainRan = true){
+return setInterval(runCarousel
+,5000)
+}
+else{
+    return 0
+}
+}
+
+// SlideShow()
 
 
-// :::::::::::Media Background Opacity on Scroll:::::::::::
+
+// ::::::::::: Background Opacity on Scroll:::::::::::
+
+
+
 function mediaBackgroundEffect(){
 window.addEventListener("scroll" , function(){
     
@@ -141,9 +164,9 @@ listeners.forEach(function(e) {
     window.addEventListener(e,function(){
     
         if(window.innerWidth < 970){
-            Onclick(contactBtn, 4150);
-            Onclick(mediaBtn, 2980);
-            Onclick(storyBtn, 950);
+            Onclick(contactBtn, 5450);
+            Onclick(mediaBtn, 2880);
+            Onclick(storyBtn, 990);
         }
         else{
             Onclick(contactBtn, 3050);
