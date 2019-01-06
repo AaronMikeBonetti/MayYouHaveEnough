b14c8068331bc2f9bb0d40a5337624696447dcc8
Aaron Bonetti <aaronmikebonetti@gmail.com>
2019-01-05 22:39:50 -0500

    #12 test: switched js around

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 //:::::::::::::::::::Nav Toggler Animation::::::::::::
 
 function navToggle(){
@@ -6,7 +5,99 @@ function navToggle(){
     mainNav.classList.toggle("nav-open")
 } 
 
+
+// ::::::::::::::::::::Navagation Scroll Effect/Responsive
+
+function navScrollButtons(){
+
+    let listeners = ['load', 'resize']
+    let contactBtn = "contact-btn"
+    let mediaBtn = "media-btn"
+    let storyBtn = "story-btn"
+    
+    function Onclick(element, y){ 
+         return document.getElementById(element).setAttribute("onclick",`window.scrollTo(0, ${y});`)} 
+    
+    listeners.forEach(function(e) {
+        window.addEventListener(e,function(){
+        
+            if(window.innerWidth < 1270){
+                Onclick(contactBtn, 5450);
+                Onclick(mediaBtn, 2880);
+                Onclick(storyBtn, 990);
+            }
+            else{
+                Onclick(contactBtn, 3050);
+                Onclick(mediaBtn, 1930);
+                Onclick(storyBtn, 900);
+             }
+        
+        })
+    });
+    }
+    // ::::::::::Timer Function for the Background Effect and for Scroll Effect
+    
+    function timer(){
+        navScrollButtons() 
+        setInterval(navScrollButtons, 1000)
+        setInterval(mediaBackgroundEffect, 5000)
+    };
+    timer()
+
+
+// ::::::::::: Background Animations on Scroll:::::::::::
+
+
+
+function mediaBackgroundEffect(){
+    window.addEventListener("scroll" , function(){
+        
+        
+        let header = document.getElementsByTagName("header")[0]; 
+        let mediaGrid = document.getElementsByClassName("media-grid")[0];
+        let headerBox = document.getElementsByClassName("header_box")[0];
+        let donateBtn= document.getElementsByClassName("donate_btn")[0];
+       
+        if(window.scrollY > 300){
+            donateBtn.classList.remove("fadeInLeft")
+            donateBtn.classList.add("fadeOutRight")
+            
+        }
+        if(window.scrollY < 300){
+            donateBtn.classList.remove("fadeOutRight")
+            donateBtn.classList.add("fadeInLeft")
+            
+        }
+        if(window.scrollY > 300){
+            headerBox.classList.add("fadeOutUp")
+            headerBox.classList.remove("fadeInDown")
+        }
+        if(window.scrollY < 300){
+            headerBox.classList.remove("fadeOutUp")
+            headerBox.classList.add("fadeInDown")
+        }
+        if(window.scrollY > 700){
+            header.classList.add("background-opacity")
+        }
+        if(window.scrollY < 700){
+            header.classList.remove("background-opacity")
+        }
+        
+       
+        if(window.scrollY < 3000){
+           mediaGrid.style.backgroundColor = "white"
+        }
+        if(window.scrollY > 3000){
+           mediaGrid.style.backgroundColor = "rgb(107, 107, 107)"
+        }
+          
+    });
+    }
+
+
 //:::::::::::::::::::Media Animations::::::::::::
+
+
 var imgsArray = [
     {url: "url('./Pics/colorado1.jpg')", id: 0},
     {url: "url('./Pics/colorado2.jpg')", id: 1},
@@ -20,11 +111,6 @@ var pushUrlToMain = url => {
     mediaMain.style.backgroundImage = `${url}`; 
     }
 var count = 0;
-
-
-
-
-
 
 function transition(element){
     element.classList.add("media-transition") 
@@ -85,89 +171,6 @@ SlideShow()
 
 
 
-// ::::::::::: Background Opacity on Scroll:::::::::::
 
 
-
-function mediaBackgroundEffect(){
-window.addEventListener("scroll" , function(){
-    
-    
-    let header = document.getElementsByTagName("header")[0]; 
-    let mediaGrid = document.getElementsByClassName("media-grid")[0];
-    let headerBox = document.getElementsByClassName("header_box")[0];
-    let donateBtn= document.getElementsByClassName("donate_btn")[0];
-   
-    if(window.scrollY > 300){
-        donateBtn.classList.remove("fadeInLeft")
-        donateBtn.classList.add("fadeOutRight")
-        
-    }
-    if(window.scrollY < 300){
-        donateBtn.classList.remove("fadeOutRight")
-        donateBtn.classList.add("fadeInLeft")
-        
-    }
-    if(window.scrollY > 300){
-        headerBox.classList.add("fadeOutUp")
-        headerBox.classList.remove("fadeInDown")
-    }
-    if(window.scrollY < 300){
-        headerBox.classList.remove("fadeOutUp")
-        headerBox.classList.add("fadeInDown")
-    }
-    if(window.scrollY > 700){
-        header.classList.add("background-opacity")
-    }
-    if(window.scrollY < 700){
-        header.classList.remove("background-opacity")
-    }
-    
-   
-    if(window.scrollY < 3000){
-       mediaGrid.style.backgroundColor = "white"
-    }
-    if(window.scrollY > 3000){
-       mediaGrid.style.backgroundColor = "rgb(107, 107, 107)"
-    }
-      
-});
-}
-// ::::::::::::::::::::Navagation Scroll Effect with Responsive Effect
-
-function navScrollButtons(){
-
-let listeners = ['load', 'resize']
-let contactBtn = "contact-btn"
-let mediaBtn = "media-btn"
-let storyBtn = "story-btn"
-
-function Onclick(element, y){ 
-     return document.getElementById(element).setAttribute("onclick",`window.scrollTo(0, ${y});`)} 
-
-listeners.forEach(function(e) {
-    window.addEventListener(e,function(){
-    
-        if(window.innerWidth < 1270){
-            Onclick(contactBtn, 5450);
-            Onclick(mediaBtn, 2880);
-            Onclick(storyBtn, 990);
-        }
-        else{
-            Onclick(contactBtn, 3050);
-            Onclick(mediaBtn, 1930);
-            Onclick(storyBtn, 900);
-         }
-    
-    })
-});
-}
-// ::::::::::Timer Function for the Background Effect and for Scroll Effect
-
-function timer(){
-    navScrollButtons() 
-    setInterval(navScrollButtons, 1000)
-    setInterval(mediaBackgroundEffect, 5000)
-};
-timer()
 
